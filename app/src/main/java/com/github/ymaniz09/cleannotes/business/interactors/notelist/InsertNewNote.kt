b629d233b88337ee2293d67d5ae9c7bbf33b13bb
Ygor <ygor.maniz@gmail.com>
2020-10-20 23:35:04 -0300

    feat: adds safeApiCall and safeCacheCall for error handling

--- app/src/main/java/com/github/ymaniz09/cleannotes/business/interactors/notelist/InsertNewNote.kt
+++ app/src/main/java/com/github/ymaniz09/cleannotes/business/interactors/notelist/InsertNewNote.kt
@@ -1,15 +1,18 @@
 package com.github.ymaniz09.cleannotes.business.interactors.notelist
 
+import com.github.ymaniz09.cleannotes.business.data.cache.CacheResponseHandler
 import com.github.ymaniz09.cleannotes.business.data.cache.abstraction.NoteCacheDataSource
 import com.github.ymaniz09.cleannotes.business.data.network.abstraction.NoteNetworkDataSource
+import com.github.ymaniz09.cleannotes.business.data.util.safeApiCall
+import com.github.ymaniz09.cleannotes.business.data.util.safeCacheCall
 import com.github.ymaniz09.cleannotes.business.domain.model.Note
 import com.github.ymaniz09.cleannotes.business.domain.model.NoteFactory
 import com.github.ymaniz09.cleannotes.business.domain.state.*
 import com.github.ymaniz09.cleannotes.framework.presentation.notelist.state.NoteListViewState
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 import java.util.*
-
 
 class InsertNewNote(
     private val noteCacheDataSource: NoteCacheDataSource,
@@ -28,45 +31,53 @@ class InsertNewNote(
             body = ""
         )
 
-        val cacheResult = noteCacheDataSource.insertNote(newNote)
-
-        var cacheResponse: DataState<NoteListViewState>? = null
-
-        if (cacheResult > 0) {
-            val viewState =
-                NoteListViewState(
-                    newNote = newNote
-                )
-
-            cacheResponse = DataState.data(
-                response = Response(
-                    message = INSERT_NOTE_SUCCESS,
-                    uiComponentType = UIComponentType.Toast(),
-                    messageType = MessageType.Success()
-                ),
-                data = viewState,
-                stateEvent = stateEvent
-            )
-        } else {
-            cacheResponse = DataState.data(
-                response = Response(
-                    message = INSERT_NOTE_FAILED,
-                    uiComponentType = UIComponentType.Toast(),
-                    messageType = MessageType.Error()
-                ),
-                data = null,
-                stateEvent = stateEvent
-            )
+        val cacheResult = safeCacheCall(Dispatchers.IO) {
+            noteCacheDataSource.insertNote(newNote)
         }
+
+        val cacheResponse = object : CacheResponseHandler<NoteListViewState, Long>(
+            response = cacheResult,
+            stateEvent = stateEvent
+        ) {
+            override suspend fun handleSuccess(resultObj: Long): DataState<NoteListViewState>? {
+                return if (resultObj > 0) {
+                    val viewState =
+                        NoteListViewState(
+                            newNote = newNote
+                        )
+                    DataState.data(
+                        response = Response(
+                            message = INSERT_NOTE_SUCCESS,
+                            uiComponentType = UIComponentType.Toast(),
+                            messageType = MessageType.Success()
+                        ),
+                        data = viewState,
+                        stateEvent = stateEvent
+                    )
+                } else {
+                    DataState.data(
+                        response = Response(
+                            message = INSERT_NOTE_FAILED,
+                            uiComponentType = UIComponentType.Toast(),
+                            messageType = MessageType.Error()
+                        ),
+                        data = null,
+                        stateEvent = stateEvent
+                    )
+                }
+            }
+        }.getResult()
 
         emit(cacheResponse)
 
-        updateNetwork(cacheResponse.stateMessage?.response?.message, newNote)
+        updateNetwork(cacheResponse?.stateMessage?.response?.message, newNote)
     }
 
     private suspend fun updateNetwork(cacheResponse: String?, newNote: Note) {
         if (cacheResponse.equals(INSERT_NOTE_SUCCESS)) {
-            noteNetworkDataSource.insertOrUpdateNote(newNote)
+            safeApiCall(Dispatchers.IO) {
+                noteNetworkDataSource.insertOrUpdateNote(newNote)
+            }
         }
     }
 
--- app/src/main/java/com/github/ymaniz09/cleannotes/util/Logger.kt
+++ app/src/main/java/com/github/ymaniz09/cleannotes/util/Logger.kt
@@ -3,6 +3,7 @@ package com.github.ymaniz09.cleannotes.util
 import android.util.Log
 import com.github.ymaniz09.cleannotes.util.Constants.DEBUG
 import com.github.ymaniz09.cleannotes.util.Constants.TAG
+import com.google.firebase.crashlytics.FirebaseCrashlytics
 
 var isUnitTest = false
 
@@ -11,5 +12,13 @@ fun printLogD(className: String?, message: String) {
         Log.d(TAG, "$className: $message")
     } else if (DEBUG && isUnitTest) {
         println("$className: $message")
+    }
+}
+
+fun cLog(msg: String?) {
+    msg?.let {
+        if (!DEBUG) {
+            FirebaseCrashlytics.getInstance().log(it)
+        }
     }
 }
